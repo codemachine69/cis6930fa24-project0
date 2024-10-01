0092b962c8226bda28e0f87021eb8545bee80fed
rohan <itsrohan999@gmail.com>
2024-10-01 19:08:25 -0400

    Changed DB name

--- project0/utils.py
+++ project0/utils.py
@@ -21,18 +21,23 @@ def extract_incidents(incident_data):
         rows = text.split('\n')
         
         for row in rows:
-            if row.startswith('Date / Time'):
+            if row.startswith('Date / Time') or row.startswith('Daily Incident Summary (Public)'):
                 continue
-            pattern = r'^(\d{1,2}/\d{1,2}/\d{4}\s\d{1,2}:\d{2})\s+(\d{4}-\d{8})\s+((?:\d+\s+)?[A-Z0-9 /\-\;\.\,]+?)(?:\s+(?=911\s)|(?=\s[A-Z][a-z]))\s*([A-Za-z0-9/\s]+)\s+([A-Z0-9]+)$'
+            # pattern = r'^(\d{1,2}/\d{1,2}/\d{4}\s\d{1,2}:\d{2})\s+(\d{4}-\d{8})\s+((?:\d+\s+)?[A-Z0-9 /\-\;\.\,]+?)(?:\s+(?=911\s)|(?=\s[A-Z][a-z]))\s*([A-Za-z0-9/\s]+)\s+([A-Z0-9]+)$'
+            pattern = r'^(\d{1,2}/\d{1,2}/\d{4}\s\d{1,2}:\d{2})\s+(\d{4}-\d{8})\s+((?:\d+\s+)?[A-Z0-9 /\-;.,]+?)(?:\s+(?=911\s)|(?=\s[A-Z][a-z])|(?=\sCOP DDACTS))\s*([A-Za-z0-9/\s]+)\s+([A-Z0-9]+)$'
+            # pattern = r'^(\d{1,2}/\d{1,2}/\d{4}\s\d{1,2}:\d{2})\s+(\d{4}-\d{8})\s+((?:\d+\s+)?[A-Z0-9 /\-;.,]+?(?:\s+Ave(?=\s))?[A-Z0-9 /\-;.,]*?)(?:\s+(?=911\s)|(?=\s[A-Z][a-z])|(?=\sCOP DDACTS))\s*([A-Za-z0-9/\s]+)\s+([A-Z0-9]+)$'
+            # pattern = r'^(\d{1,2}/\d{1,2}/\d{4}\s\d{1,2}:\d{2})\s+(\d{4}-\d{8})\s+((?:\d+\s+)?[A-Za-z0-9 /\-;.,]+?)(?:\s+(?=911\s)|(?=\s[A-Z][a-z])|(?=\sCOP DDACTS))\s*([A-Za-z0-9/\s]+)\s+([A-Z0-9]+)$'
             match = re.search(pattern, row.strip())
             if match:
                 incidents.append((match.group(1), match.group(2), match.group(3), match.group(4), match.group(5)))
+            else:
+                print('RPLOG : ' + row)
     
     return incidents
 
 def create_db():
     curr_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(curr_dir, '..', 'resources', 'incidents.db')
+    db_path = os.path.join(curr_dir, '..', 'resources', 'normanpd.db')
     
     if os.path.exists(db_path):
         os.remove(db_path)
